3cee5e50e20be1c74ccc1a279cb316efcdd143f2
Nischal Skanda <108683248+RexO77@users.noreply.github.com>
2023-08-03 08:02:16 +0530

    Update 4b.py

--- 4b.py
+++ 4b.py
@@ -1,23 +1,15 @@
-def romToDec(romstr):
-    romdict = {
-        'I' : 1,
-        'V' : 5,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'K' : 1000
-    }
-    romback = romstr[::-1]
-    val =0
-    rval = romdict[romback[0]]
-    for numaral in romback:
-        lval = romdict[numaral]
-        if lval<rval:
-            val -= lval
+def romanToDec(romStr):
+    romanDict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'K':1000}
+    romanBack = list(romStr)[::-1]
+    value = 0
+    rightVal = romanDict[romanBack[0]]
+    for numeral in romanBack:
+        leftVal = romanDict[numeral]
+        if leftVal<rightVal:
+            value-=leftVal
         else:
-            val += lval
-    rval = lval
-    return val
-n = input("Enter a Roman Value : ")
-print("Decimal Value is : ",romToDec(n))
+            value+=leftVal
+        rightVal=leftVal
+    return value
+romanStr = input("Enter a roman number :")
+print("Decimal Value : ",romanToDec(romanStr))
